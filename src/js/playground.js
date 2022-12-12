c40452ed49e89f73fbf9ec658cbbdb29b47c6c8d
MiXACT <out_lawz@mail.ru>
2022-12-12 21:06:19 +0300

    playground finished

--- src/js/playground.js
+++ src/js/playground.js
@@ -1,9 +1,19 @@
-const box = document.getElementById('box1');
-let goblinOnOff = 'box'
-const goblin = () => setTimeout(() => {
-	box.className = goblinOnOff;
-	if (goblinOnOff === 'box') goblinOnOff = 'box goblin';
-	else goblinOnOff = 'box';
-	goblin();
-}, 700);
+let prevBox;
+let boxId;
+const goblinOn = 'box goblin';
+const goblinOff = 'box'
+const goblin = () => setInterval(() => {
+	if (prevBox) goblinTrigger(prevBox, goblinOff);
+	do {
+		boxId = Math.floor(Math.random() * 16) + 1;
+	} while (prevBox === boxId);
+	prevBox = boxId;
+	goblinTrigger(boxId, goblinOn);
+}, 800);
+
+function goblinTrigger(goblinBox, status) {
+	const box = document.getElementById(`box${goblinBox}`);
+	box.className = status;
+}
+
 goblin();
